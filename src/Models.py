8e705394ed355a412b594601b56c100596032874
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2018-02-27 16:02:15 +0000

    Open Access updated

--- src/Models.py
+++ src/Models.py
@@ -8,30 +8,25 @@ import sys
 import json
 from EraConnect import getDataCiteClient    
 
-##########################################################
-#                                                        #
-# EDIT THESE!!!                                          #
-#                                                        #
-##########################################################
-model = 'rothamsted-sift-model'
-url = 'https://www.rothamsted.ac.uk/'+model
-filename = 'BROADBALK-WHEAT-YIELD-OA.json'
-
-jsonData = json.load(open('D:/Code/python/workspace/DOIs4Eradoc/dataCiteJson/'+filename))
+url = input('Enter URL: ')
+filepath = 'D:\Code\python\workspace\DOIs4Eradoc\dataCiteJson' #input('Enter filepath:')
+filename = input('Enter filename: ')
+#MODEL-MANUAL-ROTHC-WIN-VERSION-2014.json
+jsonData = json.load(open(filepath+'/'+filename))
 doi = jsonData['identifier']['identifier']
 print(doi)
 assert schema40.validate(jsonData)    
 doc = schema40.tostring(jsonData)
 print(doc)
 
-# try:
-#     d = getDataCiteClient()
-#     d.metadata_post(doc)    
-#     d.doi_post(doi, url)
-#     xname = "D:/doi_out/"+ model + ".xml"
-#     fxname = open(xname,'w+')
-#     fxname.write(doc)
-#     fxname.close()
-#     print('done')
-# except:
-#     print("Unexpected error:", sys.exc_info()[0])
+try:
+    d = getDataCiteClient()
+    d.metadata_post(doc)    
+    d.doi_post(doi, url)
+    xname = "D:/doi_out/"+ filename + ".xml"
+    fxname = open(xname,'w+')
+    fxname.write(doc)
+    fxname.close()
+    print('done')
+except:
+    print("Unexpected error:", sys.exc_info()[0])
